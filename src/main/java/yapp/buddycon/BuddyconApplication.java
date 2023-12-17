4d974207bbd44da690a069135080a6b455241a91
huey-J <wkdgus7113@gmail.com>
2023-12-17 13:06:35 +0900

    chore: dependency, gitignore 수정

--- src/main/java/yapp/buddycon/BuddyconApplication.java
+++ src/main/java/yapp/buddycon/BuddyconApplication.java
@@ -2,12 +2,16 @@ package yapp.buddycon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableJpaAuditing
+@EnableScheduling
 @SpringBootApplication
 public class BuddyconApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BuddyconApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BuddyconApplication.class, args);
+  }
 
 }
