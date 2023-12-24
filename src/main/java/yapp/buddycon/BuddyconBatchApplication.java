8165d4dd5f64e6a443ac63fc24b86e3527eeb8db
huey-J <wkdgus7113@gmail.com>
2023-12-24 17:40:46 +0900

    chore: application 이름 수정

--- src/main/java/yapp/buddycon/BuddyconBatchApplication.java
+++ src/main/java/yapp/buddycon/BuddyconBatchApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BuddyconApplication {
+public class BuddyconBatchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BuddyconApplication.class, args);
+		SpringApplication.run(BuddyconBatchApplication.class, args);
 	}
 
 }
